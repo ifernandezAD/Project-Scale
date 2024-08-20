5a16468002c915e95ce4c1ae3c582fbf76f16fec
ifernandezAD <88330098+ifernandezAD@users.noreply.github.com>
2024-08-20 17:32:00 +0200

    Menu Scene Created

---
+++ Assets/UI/Scripts/DialogueManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour
+{
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI dialogueText;  
+
+    [Header("Dialogue Settings")]
+    [TextArea(3, 10)]
+    public string[] dialogueLines;  
+    private int currentLineIndex = 0;
+
+    [SerializeField] private float typingSpeed = 0.05f;  
+
+    private bool isTyping = false;
+
+    private void Start()
+    {
+        if (dialogueLines.Length > 0)
+        {
+            StartCoroutine(TypeDialogue());
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isTyping)
+        {
+            AdvanceDialogue();
+        }
+    }
+
+    private IEnumerator TypeDialogue()
+    {
+        isTyping = true;
+        dialogueText.text = "";
+
+        foreach (char letter in dialogueLines[currentLineIndex].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
+    }
+
+    private void AdvanceDialogue()
+    {
+        if (currentLineIndex < dialogueLines.Length - 1)
+        {
+            currentLineIndex++;
+            StartCoroutine(TypeDialogue());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialogueText.text = "";  
+
+        SceneManager.LoadScene("Level1");
+    }
+}
+
